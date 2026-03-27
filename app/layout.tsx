171dfac11f0aa4e16f9f508b9e547ed327d8797c
ouattara Gnonhoua N'golo <ngolognonhoua@gmail.com>
2026-03-27 10:51:50 +0000

    correction erreur

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import './globals.css';
 
 import { inter } from '@/app/ui/fonts';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
